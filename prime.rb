0175f0b5e7d603fd9072d1e484dfa89a11f7af13
Nkenge Simms <simmsn@yahoo.com>
2020-09-03 01:11:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,6 @@
 # Add  code here!
 def prime?(n)
-  (1..n-1).none? {|divisor| n % divisor == 0 } ||
+  (1..n-1).none? {|divisor| n % divisor == 0 } 
   #(-1..n-1).none? {|divisor| n % divisor == 0 }
   
 
