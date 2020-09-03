65251a972d502b2094bdc5847ac9db3e7d036e7c
Nkenge Simms <simmsn@yahoo.com>
2020-09-03 01:39:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,7 @@ def prime?(n)
   
   return false if !number.integer?
   return false if number < 2
+  return true if number == 2
  # (2..n-1).none? {|divisor| n % divisor == 0 } 
    #if n == 2
    #else  n < 2
