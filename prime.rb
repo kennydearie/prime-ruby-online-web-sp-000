393c83df415e4a3d7d2c80ef9674f0811150c060
Nkenge Simms <simmsn@yahoo.com>
2020-09-03 01:41:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def prime?(number)
   return false if !number.integer?
   return false if number < 2
   return true if number == 2
- (2..number-1).each? {|int| return false if number % int == 0 } 
+ (2..number-1).each {|int| return false if number % int == 0 } 
  true
    #if n == 2
    #else  n < 2
