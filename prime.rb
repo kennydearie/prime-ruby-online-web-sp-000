b1519a6cc29a899d4583052d83cb70ba5d9371f4
Nkenge Simms <simmsn@yahoo.com>
2020-09-03 01:32:14 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 # Add  code here!
 def prime?(n)
-  (2..n-1).none? {|divisor| n % divisor == 0 } 
-  #(-1..n-1).none? {|divisor| n % divisor == 0 }
+  #(2..n-1).none? {|divisor| n % divisor == 0 } 
+  return true if n == 2
   return false if  n < 2
 
 
